88abfc03331e1571fd9fe364fa08248cd9c3c807
schwana <32838066+schwana@users.noreply.github.com>
2019-03-13 12:31:05 +0000

    Removed a lot of code

--- TuneUp.py
+++ TuneUp.py
@@ -1,31 +1,7 @@
 import time
 import socket
 
-global spectrum
-rS_=[]
-iE_=[]
-L5_=[]
-L4_=[]
-L3_=[]
-L2_=[]
-L1_=[]
-Ax_=[]
-H1_=[]
-H2_=[]
-H3_=[]
-H4_=[]
-global iE
-global L5
-global L4
-global L3
-global L2
-global L1
-global Ax
-global H1
-global H2
-global H3
-global H4
-global N
+
 
 print ("Peak Centering")
 PeakToScan=1390
@@ -70,7 +46,7 @@ SVStr=("SetSourceOutput IE,"+str(IE)+"\r\n")
 s.send(str.encode(SVStr))
 time.sleep(0.1)
 IEreturn=(s.recv(1024))
-time.sleep(5)
+time.sleep(2)
 
 while (IE<(EndIE+1)):
     
@@ -80,127 +56,33 @@ while (IE<(EndIE+1)):
     IEreturn=(s.recv(1024))
     time.sleep(0.1)
 
-
-
     #Acquire Data - wait for enough time for the buffer to fill....
     
     s.send(b'StartAcq 1,JS\r\n')
     time.sleep((acqRestTime))
     returnString=s.recv(1024)
     time.sleep(0.1)
-
-    rS_String=(","+str(0.0) + ","+str(0.0)+ ","+str(0.0)+ "," +
-                str(0.0)+ "," +
-                str(0.0)+ "," +
-                str(0.0)+ "," +
-                str(0.0)+ "," +
-                str(0.0)+ "," +
-                str(0.0)+ "," +
-                str(0.0)+ ",")
-
-    #Separate the string
+   #Separate the string
     spec=(returnString.decode("utf-8"))
-    #print(spec)
-
-    
-    tempStr=spec.split('#')
-    
-    temp_i=1
-    data_N=0
-    SpecIntegration=[]
-    StringStart=""
-    while (temp_i<len(tempStr)):
-        SpecIntegration.append(tempStr[temp_i])
-        data_N=data_N+1
-        temp_i=temp_i+2
-    tempStr2=tempStr[1].split(',')
-
-    StringStart=(tempStr2[0]+','+
-                 tempStr2[1]+','+
-                 tempStr2[2]+','+
-                 tempStr2[3]+','+
-                 tempStr2[4]+',')
-    
-    temp_col=5
-    averagedData=[]
-    #Loop through the columns and integrations to
-    #average out the data.
-    #Currently just the average (no std dev) is used.
-
-    #Loop through channels
-    while (temp_col<16):
-        running=0.0
-        data_N=0
-        
-        #Loop through integrations
-        while (data_N<len(SpecIntegration)):
-            dummyString=SpecIntegration[data_N].split(',')
-            running=running+float(dummyString[temp_col])
-            data_N=data_N+1
-        
-        running=running/len(SpecIntegration)
-        averagedData.append(running)
-        temp_col=temp_col+1
-        
-
-
-
-    #Add the averaged data to a string.
-    temp_i=0
-    EndString=""
-    while (temp_i<len(averagedData)):
-
-           EndString=(EndString+str(averagedData[temp_i])+',')
-           temp_i=temp_i+1
-
-    EndString=EndString[:-1]
-
-
-    #Reform the data string to match that created by the mass
-    #spec acquisition return
-    spec2='#'+StringStart+EndString+'#'
-    
-    #Add the Ion Source voltage to the start of the string
-    rS=(str(IE)+","+spec2)
-    rS=rS[0:-1]
+    rS=(str(SV_Peak+SV_BL)+","+spec)
+    rS=rS[0:-5]
     spectrum=rS.split(',')
 
     #print (spectrum)
     #Add the inidividual readings to the relevent array
     #for data output and plotting
-    rS_.append(rS_String)
+    
     iE_.append(float(spectrum[0]))
-    L5_.append(0.0)
-    L4_.append(0.0)
-    L3_.append(0.0)
-    L2_.append(0.0)
-    L1_.append(0.0)
     Ax_.append(float(spectrum[13]))
-    H1_.append(0.0)
-    H2_.append(0.0)
-    H3_.append(0.0)
-    H4_.append(0.0)
 
     iE=iE_
-    L5=L5_
-    L4=L4_
-    L3=L3_
-    L2=L2_
-    L1=L1_
     Ax=Ax_
-    H1=H1_
-    H2=H2_
-    H3=H3_
-    H4=H4_
-    rS=rS_
-
     N=len(iE)
    
     IE=IE+1
 
     print(float(spectrum[13]))
     
-
 s.close()
 
 
@@ -247,14 +129,4 @@ Offset= PeakToScan-PeakCentre
 
 #Reset Arrays
 iE.clear()
-L5.clear()
-L4.clear()
-L3.clear()
-L2.clear()
-L1.clear()
 Ax.clear()
-H1.clear()
-H2.clear()
-H3.clear()
-H4.clear()
-rS_.clear()        
