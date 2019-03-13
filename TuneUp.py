093de3fe2bba7427897c3423d24db0bf6a4d54ea
schwana <32838066+schwana@users.noreply.github.com>
2019-03-13 15:08:32 +0000

    Added in remainder of tuning factors

--- TuneUp.py
+++ TuneUp.py
@@ -58,8 +58,14 @@ print ("Peak Tune Up")
 StartIE=1300
 EndIE=1500
 
-StartIR=-10
+StartIR=7
 EndIR=8
+
+StartYB=6
+EndYB=7
+
+StartYF=69
+EndYF=70
 
 #Connect to instrument
 print ("Connecting to mass spec")
@@ -95,137 +101,157 @@ acqRestTime=0.2
 
 
 ## Loop through IR, YB,YF and then scan
-IR=StartIR
 
-
-while (IR<(EndIR+1)):
-
-    
-    IRStr=("SetSourceOutput IR,"+str(IR)+"\r\n")
-    s.send(str.encode(IRStr))
+YF=StartYF
+while (YF<(EndYF+1)):
+    YFStr=("SetSourceOutput YF,"+str(YF)+"\r\n")
+    s.send(str.encode(YFStr))
     time.sleep(0.1)
-    IRreturn=(s.recv(1024))
+    YFreturn=(s.recv(1024))
     time.sleep(1)
 
-    IE=StartIE
-    SVStr=("SetSourceOutput IE,"+str(IE)+"\r\n")
-    s.send(str.encode(SVStr))
-    time.sleep(0.1)
-    IEreturn=(s.recv(1024))
-    time.sleep(2)
+    YB=StartYB
+    while (YB<(EndYB+1)):
 
-    while (IE<(EndIE+1)):
-        
-        SVStr=("SetSourceOutput IE,"+str(IE)+"\r\n")
-        s.send(str.encode(SVStr))
+        YBStr=("SetSourceOutput YB,"+str(YB)+"\r\n")
+        s.send(str.encode(YBStr))
         time.sleep(0.1)
-        IEreturn=(s.recv(1024))
-        time.sleep(0.1)
+        YBreturn=(s.recv(1024))
+        time.sleep(1)
 
-        #Acquire Data - wait for enough time for the buffer to fill....
-        
-        s.send(b'StartAcq 1,JS\r\n')
-        time.sleep((acqRestTime))
-        returnString=s.recv(1024)
-        time.sleep(0.1)
-       #Separate the string
-        spec=(returnString.decode("utf-8"))
-        rS=(str(IE)+","+spec)
-        rS=rS[0:-5]
-        spectrum=rS.split(',')
+        IR=StartIR
+        while (IR<(EndIR+1)):
 
-        #print (spectrum)
-        #Add the inidividual readings to the relevent array
-        #for data output and plotting
-        
-        iE_.append(float(spectrum[0]))
-        Ax_.append(float(spectrum[13]))
-
-        iE=iE_
-        Ax=Ax_
-        N=len(iE)
-       
-        IE=IE+1
-
-       # print(float(spectrum[13]))
-
-
-        
-
-    #Calculate the peak centre
-    #Need iE and Ax for this.
-
-    MaxSignal = (max(Ax))
-    MinSignal = (min(Ax))
-
-    HalfPeakHeight=(MaxSignal-MinSignal)/2
-
-    if (HalfPeakHeight>0.01):
-        #Loop beween Start IE and midIE to look for voltage
-        #where Ax is HalfPeakheight
-        TestVoltage=StartIE
-        i=0
-        while (TestVoltage<EndIE):
-
-            if (Ax[i]>HalfPeakHeight):
-                FWHM_Left=iE[i]
-                break
-            i=i+1
-            TestVoltage=TestVoltage+1
-
-        TestVoltage=StartIE
-        i=0
-        while (TestVoltage<EndIE):
-
-            if(TestVoltage>(FWHM_Left+20)):
-
-                if (Ax[i]<HalfPeakHeight):
-                    FWHM_Right=iE[i]
-                    break
-            i=i+1
-            TestVoltage=TestVoltage+1
             
-        print("Left ",FWHM_Left)
-        print("Right ",FWHM_Right)
+            IRStr=("SetSourceOutput IR,"+str(IR)+"\r\n")
+            s.send(str.encode(IRStr))
+            time.sleep(0.1)
+            IRreturn=(s.recv(1024))
+            time.sleep(1)
 
-        PeakCentre=((FWHM_Right-FWHM_Left)/2)+FWHM_Left
+            IE=StartIE
+            SVStr=("SetSourceOutput IE,"+str(IE)+"\r\n")
+            s.send(str.encode(SVStr))
+            time.sleep(0.1)
+            IEreturn=(s.recv(1024))
+            time.sleep(2)
 
-        print ("Centre ",PeakCentre)
+            while (IE<(EndIE+1)):
+                
+                SVStr=("SetSourceOutput IE,"+str(IE)+"\r\n")
+                s.send(str.encode(SVStr))
+                time.sleep(0.1)
+                IEreturn=(s.recv(1024))
+                time.sleep(0.1)
 
-        #Get Hi and Low
-        intCentre = int(PeakCentre)
+                #Acquire Data - wait for enough time for the buffer to fill....
+                
+                s.send(b'StartAcq 1,JS\r\n')
+                time.sleep((acqRestTime))
+                returnString=s.recv(1024)
+                time.sleep(0.1)
+               #Separate the string
+                spec=(returnString.decode("utf-8"))
+                rS=(str(IE)+","+spec)
+                rS=rS[0:-5]
+                spectrum=rS.split(',')
 
-        Lo = intCentre-15
-        Hi = intCentre+15
+                #print (spectrum)
+                #Add the inidividual readings to the relevent array
+                #for data output and plotting
+                
+                iE_.append(float(spectrum[0]))
+                Ax_.append(float(spectrum[13]))
 
-        #Search iE to get the index of Hi and Lo
+                iE=iE_
+                Ax=Ax_
+                N=len(iE)
+               
+                IE=IE+1
 
-        iLo = iE.index(Lo)
-        iHi = iE.index(Hi)
-        iCr = iE.index(intCentre)
+               # print(float(spectrum[13]))
 
-        LowSig=Ax[iLo]
-        HighSig=Ax[iHi]
-        CentSig=Ax[iCr]
 
-        Roundness = (CentSig - ((HighSig+LowSig)/2))/CentSig
+                
 
-        PSF = 1/Roundness
-    else:
-        HighSig=0
-        Roundness=0
-        PSF=0
+            #Calculate the peak centre
+            #Need iE and Ax for this.
 
-    #Output to file
-    #Open and append summary
-    print ("Filename,YF,YB,",IR,HighSig,PSF)
-    #Create and save scan
-    outputData(iE,Ax)
-    #Reset Arrays
-    iE.clear()
-    Ax.clear()
+            MaxSignal = (max(Ax))
+            MinSignal = (min(Ax))
 
-    IR=IR+1
+            HalfPeakHeight=(MaxSignal-MinSignal)/2
+
+            if (HalfPeakHeight>0.01):
+                #Loop beween Start IE and midIE to look for voltage
+                #where Ax is HalfPeakheight
+                TestVoltage=StartIE
+                i=0
+                while (TestVoltage<EndIE):
+
+                    if (Ax[i]>HalfPeakHeight):
+                        FWHM_Left=iE[i]
+                        break
+                    i=i+1
+                    TestVoltage=TestVoltage+1
+
+                TestVoltage=StartIE
+                i=0
+                while (TestVoltage<EndIE):
+
+                    if(TestVoltage>(FWHM_Left+20)):
+
+                        if (Ax[i]<HalfPeakHeight):
+                            FWHM_Right=iE[i]
+                            break
+                    i=i+1
+                    TestVoltage=TestVoltage+1
+                    
+                print("Left ",FWHM_Left)
+                print("Right ",FWHM_Right)
+
+                PeakCentre=((FWHM_Right-FWHM_Left)/2)+FWHM_Left
+
+                print ("Centre ",PeakCentre)
+
+                #Get Hi and Low
+                intCentre = int(PeakCentre)
+
+                Lo = intCentre-15
+                Hi = intCentre+15
+
+                #Search iE to get the index of Hi and Lo
+
+                iLo = iE.index(Lo)
+                iHi = iE.index(Hi)
+                iCr = iE.index(intCentre)
+
+                LowSig=Ax[iLo]
+                HighSig=Ax[iHi]
+                CentSig=Ax[iCr]
+
+                Roundness = (CentSig - ((HighSig+LowSig)/2))/CentSig
+
+                PSF = 1/Roundness
+            else:
+                HighSig=0
+                Roundness=0
+                PSF=0
+
+            #Output to file
+            #Open and append summary
+            print ("Filename,YF,YB,",IR,HighSig,PSF)
+            #Create and save scan
+            #outputData(iE,Ax)
+            #Reset Arrays
+            iE.clear()
+            Ax.clear()
+
+            IR=IR+1
+
+        YB=YB+1
+        
+    YF=YF=1
 
 
 
